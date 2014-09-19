a6e39654f4410a73df4dca8cba67ce95abc823da
Daniel Qian <chanjarster@gmail.com>
2014-09-19 10:26:41 +0800

    update

---
+++ src/test/java/me/chanjar/CallActivityTest.java
@@ -0,0 +1,69 @@
+package me.chanjar;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:springTypicalUsageTest-context.xml")
+public class CallActivityTest {
+    
+    @Autowired
+    private RuntimeService runtimeService;
+    
+    @Autowired
+    private TaskService taskService;
+    
+    @Autowired
+    @Rule
+    public ActivitiRule activitiSpringRule;
+    
+    @Autowired
+    public RepositoryService repositoryService;
+    
+    @Test
+    @Deployment(resources={"me/chanjar/call-activity-main.bpmn", "me/chanjar/call-activity-sub.bpmn"})
+    public void test() throws InterruptedException {
+      String mainProcessDefinitionKey = "call-activity-main";
+      String subProcessDefinitionKey = "call-activity-sub";
+      Map<String, Object> variables = new HashMap<String, Object>();
+      /*
+       * 在这里设置了a_in_sub是没用的，因为parent process instance里的变量名
+       * 是不共享到sub process instance的
+       */
+      variables.put("a_in_main", Boolean.TRUE);
+      // variables.put("a_in_sub", Boolean.TRUE);
+
+      runtimeService.startProcessInstanceByKey(mainProcessDefinitionKey, variables);
+      
+      // 当走到call activity的时候，会产生一条新的process instance，而这个process instance是属于call activity的
+      assertEquals(2, runtimeService.createProcessInstanceQuery().count());
+
+      // 完成一个任务
+      Task task = taskService
+            .createTaskQuery()
+            // 必须是根据sub process的definition key来查找task
+            .processDefinitionKey(subProcessDefinitionKey)
+            .taskDefinitionKey("usertask1")
+            .singleResult();
+      taskService.complete(task.getId());
+    
+      // 判断process instance已经结束
+      assertEquals(0, runtimeService.createProcessInstanceQuery().processDefinitionKey(mainProcessDefinitionKey).count());
+    }
+    
+}
---
+++ src/test/java/me/chanjar/TimerEventLazyTest.java
@@ -0,0 +1,81 @@
+package me.chanjar;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:springTypicalUsageTest-context.xml")
+public class TimerEventLazyTest {
+    
+    @Autowired
+    private RuntimeService runtimeService;
+    
+    @Autowired
+    private TaskService taskService;
+    
+    @Autowired
+    @Rule
+    public ActivitiRule activitiSpringRule;
+    
+    @Autowired
+    public RepositoryService repositoryService;
+    
+    @Test(expected=org.activiti.engine.ActivitiException.class)
+    public void timerStartEvent() throws InterruptedException {
+      repositoryService.createDeployment().addClasspathResource("me/chanjar/timer-start-event-lazy.bpmn").deploy();
+    }
+    
+    @Test
+    @Deployment(resources="me/chanjar/timer-intermediate-catch-event-lazy.bpmn")
+    public void timerIntermediateCatchEvent() throws InterruptedException {
+      String processDefinitionKey = "timer-intermediate-catch-event-lazy";
+      Map<String, Object> variables = new HashMap<String, Object>();
+      variables.put("duration", "P0Y0M0DT0H0M5S");
+      runtimeService.startProcessInstanceByKey(processDefinitionKey, variables);
+      
+      Thread.sleep(10 * 1000);
+      // 完成一个任务
+      Task task = taskService.createTaskQuery().processDefinitionKey(processDefinitionKey).taskDefinitionKey("usertask1").singleResult();
+      taskService.complete(task.getId());
+    
+      // 判断process instance已经结束
+      assertEquals(0, runtimeService.createProcessInstanceQuery().processDefinitionKey(processDefinitionKey).count());
+    }
+    
+    @Test
+    @Deployment(resources="me/chanjar/timer-intermediate-catch-event-lazy-2.bpmn")
+    public void timerIntermediateCatchEvent2() throws InterruptedException {
+      String processDefinitionKey = "timer-intermediate-catch-event-lazy-2";
+      runtimeService.startProcessInstanceByKey(processDefinitionKey);
+      
+      Map<String, Object> variables = new HashMap<String, Object>();
+      variables.put("duration", "P0Y0M0DT0H0M5S");
+      
+      Task task1 = taskService.createTaskQuery().processDefinitionKey(processDefinitionKey).taskDefinitionKey("usertask1").singleResult();
+      taskService.complete(task1.getId(), variables);
+      
+      Thread.sleep(10 * 1000);
+      // 完成一个任务
+      Task task2 = taskService.createTaskQuery().processDefinitionKey(processDefinitionKey).taskDefinitionKey("usertask2").singleResult();
+      taskService.complete(task2.getId());
+    
+      // 判断process instance已经结束
+      assertEquals(0, runtimeService.createProcessInstanceQuery().processDefinitionKey(processDefinitionKey).count());
+    }
+    
+}
